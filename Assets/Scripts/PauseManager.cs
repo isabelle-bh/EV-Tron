81d257bd1d64907c02879374019d4460cbc4ee62
Isabelle Beaudry Hajji <100730620+isabelle-bh@users.noreply.github.com>
2025-10-21 21:58:30 -0400

    feat: Pause menu functionality in game

---
+++ Assets/Scripts/PauseManager.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseManager : MonoBehaviour
+{
+    public GameObject pauseUI;
+    //public GameObject musicUI;
+    public bool isPaused = false;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape) && isPaused == false)
+        {
+            Pause();
+        }
+        else if (Input.GetKeyDown(KeyCode.Escape) && isPaused == true)
+        {
+            UnPause();
+        }
+    }
+
+    public void Pause()
+    {
+        Time.timeScale = 0;
+        pauseUI.SetActive(true);
+        //musicUI.SetActive(false);
+        isPaused = true;
+    }
+    public void UnPause()
+    {
+        Time.timeScale = 1;
+        pauseUI.SetActive(false);
+        //musicUI.SetActive(true);
+        isPaused = false;
+    }
+
+    public void BackToMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
